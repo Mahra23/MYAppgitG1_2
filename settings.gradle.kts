79bc8e0a3171a95c5c8dd7cbb986f93130a755bb
mahra <201821018@gcet.edu.om>
2024-05-01 12:38:18 +0400

    Finished project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My AppgitG1_2"
+include(":app")
+ 
